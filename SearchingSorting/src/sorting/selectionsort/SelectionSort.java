ad8d8ff3b8b91cfbf8b91b9bafdc31db9477d154
bitangsha2014@gmail.com <bitangsha2014@gmail.com>
2024-01-04 09:58:07 +0530

    commiting again

--- SearchingSorting/src/sorting/selectionsort/SelectionSort.java
+++ SearchingSorting/src/sorting/selectionsort/SelectionSort.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SelectionSort {
 	
 	public static void main(String[] args) {
-		int[] arr = {10,3,1,9,5,8,6,4,2,7}; 
+		int[] arr = {7,4,1,8}; 
 		print(sort(arr));
 		
 	}
@@ -14,7 +14,7 @@ public class SelectionSort {
 		int max,temp,maxIndex = 0;
 		for(int i=0; i<arr.length; i++) {
 			max=Integer.MIN_VALUE;
-			print(arr);
+			//print(arr);
 			for(int j=0; j<arr.length-i; j++) {
 				
 				max=Math.max(max, arr[j]);
@@ -23,8 +23,8 @@ public class SelectionSort {
 			}
 			
 			if(arr[arr.length-i-1]!=max) {
-				System.out.println("Max : " + max);
-				System.out.println("Swapping : " + arr[maxIndex] + " and " + arr[arr.length-i-1]);
+				//System.out.println("Max : " + max);
+				//System.out.println("Swapping : " + arr[maxIndex] + " and " + arr[arr.length-i-1]);
 				temp=arr[maxIndex];
 				arr[maxIndex]=arr[arr.length-i-1];
 				arr[arr.length-i-1]=temp;				
